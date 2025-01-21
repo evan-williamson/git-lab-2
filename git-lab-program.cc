71ee48b3170854a1a87c22845f4cc2cfddec8d7e
Evan Williamson <ew146023@ohio.edu>
2025-01-21 11:06:21 -0500

    Hello git program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Evan Williamson
+ *        Date: 1/21/25
+ * Description: Program to say hello to git!
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
